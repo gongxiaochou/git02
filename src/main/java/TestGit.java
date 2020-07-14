ddfed595aa419065f929f3ebf5079d45ae44b497
gongxiaochou <51401597+gongxiaochou@users.noreply.github.com>
2020-07-14 09:00:46 +0800

    Update TestGit.java

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,7 +2,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("test success");
         System.out.println("再试一次");
-        System.out.println("董玲玲");
+        System.out.println("ok");
         System.out.println("测试冲突");
         System.out.println("丰田亚洲龙");
         System.out.println("丰田凯美瑞");
